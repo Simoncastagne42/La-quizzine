ed96d3c788a51d3632da1b4ced8a23fe1c45318f
gaksss <clement.carlier2@gmail.com>
2024-12-13 11:39:23 +0100

    Export DB

--- quizz.php
+++ quizz.php
@@ -1,11 +1,12 @@
 <?php
 require_once './utils/connect_db.php';
 
-$sql = "SELECT * FROM `answer`";
+$idQuestion = 1;
+$sql = "SELECT * FROM `answer` WHERE idQuestion = $idQuestion";
 
 try {
     $stmt = $pdo->query($sql);
-    $users = $stmt->fetchAll(PDO::FETCH_ASSOC); // ou fetch si vous savez que vous n'allez avoir qu'un seul résultat
+    $answers = $stmt->fetchAll(PDO::FETCH_ASSOC); // ou fetch si vous savez que vous n'allez avoir qu'un seul résultat
 
 } catch (PDOException $error) {
     echo "Erreur lors de la requete : " . $error->getMessage();
@@ -17,15 +18,17 @@ try {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>La Quizzine</title>
     <link rel="stylesheet" href="./style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&family=Lobster&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&family=Lobster&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <header>
         <h1>La Quizzine</h1>
@@ -35,16 +38,18 @@ try {
         <section id="section-quizz">
             <h2>Qui Renaud a embrassé ?</h2>
             <article id="article-quizz">
-            <div class="reponse">Un pompier</div>
-            <div class="reponse">Un infirmier</div>
-            <div class="reponse">Un flic</div>
-            <div class="reponse">Un militaire</div>
-        </article>
-        <div id="pagination">3/10</div>
-        
+
+                <?php foreach ($answers as $answer) {
+                    echo '<div class="reponse">' . $answer['textReponse'] . "</div>";
+                } ?>
+            </article>
+            <div id="pagination">3/10</div>
+
         </section>
+
     </main>
 
 
 </body>
+
 </html>
--- sql/la_quizzine.sql
+++ sql/la_quizzine.sql
@@ -1,24 +1,134 @@
-CREATE TABLE `question`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `questionName` VARCHAR(255) NOT NULL,
-    `idTheme` BIGINT NOT NULL
-);
-CREATE TABLE `answer`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `idQuestion` INT NOT NULL,
-    `textReponse` VARCHAR(255) NOT NULL,
-    `isCorrect` BOOLEAN NOT NULL
-);
-CREATE TABLE `user`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `pseudo` VARCHAR(255) NOT NULL,
-    `score` INT NOT NULL
-);
-CREATE TABLE `theme`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `themeName` VARCHAR(255) NOT NULL
-);
-ALTER TABLE
-    `question` ADD CONSTRAINT `question_idtheme_foreign` FOREIGN KEY(`idTheme`) REFERENCES `theme`(`id`);
-ALTER TABLE
-    `answer` ADD CONSTRAINT `answer_idquestion_foreign` FOREIGN KEY(`idQuestion`) REFERENCES `question`(`id`);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : ven. 13 déc. 2024 à 10:38
+-- Version du serveur : 9.1.0
+-- Version de PHP : 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `la_quizzine`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `answer`
+--
+
+DROP TABLE IF EXISTS `answer`;
+CREATE TABLE IF NOT EXISTS `answer` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `idQuestion` int NOT NULL,
+  `textReponse` varchar(255) NOT NULL,
+  `isCorrect` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `answer_idquestion_foreign` (`idQuestion`)
+) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `answer`
+--
+
+INSERT INTO `answer` (`id`, `idQuestion`, `textReponse`, `isCorrect`) VALUES
+(1, 1, 'Un pompier?', 0),
+(2, 1, 'Un infirmier?', 0),
+(3, 1, 'Un flic?', 1),
+(4, 1, 'Un militaire?', 0),
+(5, 2, 'Un oiseau?', 0),
+(6, 2, 'Un 747?', 1),
+(7, 2, 'Un ecureuil?', 0),
+(8, 2, 'Un 38 tonnes?', 0),
+(9, 3, '4L?', 0),
+(10, 3, '3L?', 0),
+(11, 3, '2L?', 0),
+(12, 3, '1L?', 1),
+(13, 4, 'Un café noir?', 1),
+(14, 4, 'Un ricard?', 0),
+(15, 4, 'Un jambon-beurre?', 0),
+(16, 4, 'Un whisky coca?', 0),
+(17, 5, 'Mistral Gagnant?', 0),
+(18, 5, 'Dès que le vent soufflera?', 1),
+(19, 5, 'Manatthan Kaboul?', 0),
+(20, 5, 'Laisse Béton?', 0),
+(21, 6, '2h?', 0),
+(22, 6, 'Une journée?', 0),
+(23, 6, '5 minutes?', 1),
+(24, 6, '5h?', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
+CREATE TABLE IF NOT EXISTS `question` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `questionName` varchar(255) NOT NULL,
+  `idTheme` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `question_idtheme_foreign` (`idTheme`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `question`
+--
+
+INSERT INTO `question` (`id`, `questionName`, `idTheme`) VALUES
+(1, 'Qui Renaud a embrassé?', 2),
+(2, 'Qu\'est ce qui s\'est ecrasé dans ses fenêtres?', 2),
+(3, 'Combien de litres de Pastis Renaud buvait-il par jour?', 2),
+(4, 'Qu\'est ce qu\'a commandé le type dans le bar dans \"Laisse béton\"?', 2),
+(5, 'Quelle chanson de Renaud commence par les paroles : \"C\'est pas l\'homme qui prend la mer, c\'est la mer qui prend l\'homme\" ?', 2),
+(6, 'Dans Mistral Gagnant, combien de temps Renaud s\'assoit sur un banc?', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `theme`
+--
+
+DROP TABLE IF EXISTS `theme`;
+CREATE TABLE IF NOT EXISTS `theme` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `themeName` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `theme`
+--
+
+INSERT INTO `theme` (`id`, `themeName`) VALUES
+(2, 'Renaud');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(255) NOT NULL,
+  `score` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
